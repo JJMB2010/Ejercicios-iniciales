322935d4829f9c232648cd138982ab39dbd2cec5
Hornet <juanjose.marin@campus.xaloc.org>
2025-11-05 09:53:41 +0100

    inicio

---
+++ Ejercicios bucles/ex35.py
@@ -0,0 +1,6 @@
+# 35. Programa que al introducir un número por teclado permita mostrar ese número de veces tu nombre.
+
+numb=int(input("Dime un numero: "))
+
+for x in range(numb):
+    print("Juanjo")
---
+++ Ejercicios bucles/ex36.py
@@ -0,0 +1,9 @@
+# 36. Programa que sume los n primeros números naturales. n Lo introduce el usuario.
+
+numb=int(input("Dime un numero: "))
+n=int(0)
+
+for a in (numb):
+    n=n+n+1
+
+print(n)
