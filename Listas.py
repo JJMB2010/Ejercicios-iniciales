20fb7ca3e76e589955b8a52b74c1f91712ff7887
Hornet <juanjose.marin@campus.xaloc.org>
2026-01-09 16:02:58 +0100

    sisisisisi

---
+++ Listas.py
@@ -0,0 +1,24 @@
+milista=[1,2,3,4,5,6]
+milistapordos=[]
+milistacopiada=[]
+
+#maximo=max(milista)
+#minimo=min(milista)
+#suma=sum(milista)
+
+#print(milista)
+#print("maximo:",maximo)
+#print("minimo:",minimo)
+#print("suma:",suma)
+
+#for x in range (0,len(milista)):
+#    a=milista[x]*2
+#    milistapordos.append(a)
+
+#print(milistapordos)
+
+#for x in milista:
+#    milistapordos.append(x*2)
+
+#print(milistapordos)
+
