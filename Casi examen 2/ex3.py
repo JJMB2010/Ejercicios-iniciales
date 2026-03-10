f8e9ca9743d3249f334558c475eb678a157f897a
Hornet <juanjose.marin@campus.xaloc.org>
2026-03-10 19:34:47 +0100

    fiansas

--- Casi examen 2/ex3.py
+++ Casi examen 2/ex3.py
@@ -1,5 +1,35 @@
 lista=input().split("-")
-listalista=lista.split(":")
+maximo=0
 lista_producto=[]
 lista_precio=[]
 lista_stock=[]
+nada=[]
+suma=0
+
+for x in range(len(lista)):
+    listalista=lista[x].split(":")
+    lista_producto.append(listalista[0])
+    lista_precio.append(listalista[1])
+    lista_stock.append(listalista[2])
+    suma+=int(listalista[1])*int(listalista[2])
+maximo=max(lista_precio)
+b=lista_precio.index(maximo)
+caro=lista_producto[b]
+
+ñ=0
+z=0
+for y in lista_stock:
+    if int(y)==0:
+        nada.append(lista_producto[int(z)-int(ñ)])
+        lista_producto.pop(int(z)-int(ñ))
+        lista_precio.pop(int(z)-int(ñ))
+        lista_stock.pop(int(z)-int(ñ))
+        ñ+=1
+    z+=1
+
+print(lista_producto)
+print(lista_precio)
+print(lista_stock)
+print(nada)
+print(suma)
+print(caro)
